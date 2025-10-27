dabadc1ce837913cefd3c1b3bde2ef91ccdbfe7b
Anuj Thakur <anujthakur2004@gmail.com>
2025-10-27 19:31:32 +0530

    day1 new

--- fashionhub/fashionhub/urls.py
+++ fashionhub/fashionhub/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path('home', home, name='home'),
 
     path("__reload__/", include("django_browser_reload.urls")),
     ]
