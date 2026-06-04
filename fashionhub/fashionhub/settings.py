12074023d6d55ba31b891a1662123be72e90af6d
Anuj Thakur <anujthakur2004@gmail.com>
2026-06-04 13:52:38 +0530

    c

--- fashionhub/fashionhub/settings.py
+++ fashionhub/fashionhub/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uo&mk73u#kuj(4+=-+xwu6mwtcm=05-6@a=uhy+9w40!i0q--r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django_browser_reload.middleware.BrowserReloadMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'fashionhub.urls'
